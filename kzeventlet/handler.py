d5fc78ca629b78374bacca5f6fa12e3e176f2d57
Ted Kaemming <ted@kaemming.com>
2012-11-08 15:54:04 -0800

    don't need async result alias

--- kzeventlet/handler.py
+++ kzeventlet/handler.py
@@ -3,8 +3,6 @@ from pqueue import PeekableQueue
 
 threading = eventlet.import_patched('kazoo.handlers.threading')
 
-AsyncResult = threading.AsyncResult
-
 
 class SequentialEventletHandler(threading.SequentialThreadingHandler):
     sleep_func = staticmethod(eventlet.sleep)
