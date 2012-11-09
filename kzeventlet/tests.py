1640f58a8b3d7aeb30ad96e9ed920ac38d48edb4
Ted Kaemming <ted@kaemming.com>
2012-11-08 16:49:18 -0800

    notes

--- kzeventlet/tests.py
+++ kzeventlet/tests.py
@@ -9,14 +9,23 @@ from kzeventlet.handler import SequentialEventletHandler
 
 
 class EventletKazooClient(KazooClient):
+    """
+    A KazooClient that is bound to using the eventlet handler.
+    """
     def setUp(self, *args, **kwargs):
         kwargs['handler'] = SequentialEventletHandler()
         return super(EventletKazooClient, self).super(*args, **kwargs)
 
 
 class EventletTestCaseMixin(object):
+    """
+    A mixin for Kazoo test cases that forces the test runner to only use the
+    client with SequentialEventletHandler.
+    """
     @mock.patch('kazoo.client.KazooClient')
     def expire_session(self, mock_client, *args, **kwargs):
+        # Force using the :class:`SequentialEventletHandler` instead of threaded,
+        # this method doesn't actually call :meth:`_get_client`.
         mock_client.return_value = SequentialEventletHandler.__init__
         return super(EventletTestCaseMixin, self).expire_session(*args, **kwargs)
 
